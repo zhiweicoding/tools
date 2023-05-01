d2a152658c5765390ad78a24c3a7a31650e05503
zhiweicoding <diaozhiwei2k@163.com>
2023-05-01 13:30:33 +0800

    init

---
+++ sms/src/main/java/xyz/zhiweicoding/Main.java
@@ -0,0 +1,11 @@
+package xyz.zhiweicoding;
+
+/**
+ * @author by diaozhiwei on 2023/05/01.
+ * @email diaozhiwei2k@163.com
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
